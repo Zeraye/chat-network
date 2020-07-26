54985608ec2d7102ac667b46095489ee3f09a0fa
Jakub Rudnik <jakubrudnik02@gmail.com>
2020-07-26 23:32:08 +0200

    update user

--- user.py
+++ user.py
@@ -1,10 +1,9 @@
-import pygame_textinput
-import pygame
-
-
 class User:
-    def __init__(self, name, perms, conv, words=0):
+    def __init__(self, name, number, perms='USER', conv=None, words=0):
+        if conv is None:
+            conv = []
         self.name = name
+        self.number = number
         self.perms = perms
         self.words = words
         self.conv = conv
@@ -12,8 +11,17 @@ class User:
     def get_name(self):
         return self.name
 
+    def change_name(self, new_name):
+        self.name = new_name
+
+    def get_number(self):
+        return self.number
+
     def get_perms(self):
         return self.perms
+
+    def change_perms(self, new_perms):
+        self.name = new_perms
 
     def add_word(self):
         self.words += 1
